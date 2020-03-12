d0fcea20d5645752ea30342fc62333ff06289e54
Robu Waldorf <fieldassociationrecording@gmail.com>
2020-03-12 12:11:45 -0400

    Installed Sequelize

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+const layout = require("./views/layout");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
 
+
 app.get("/", (req, res, next) => {
-    res.send('HelloWorld');
+    res.send(layout('hellohello'));
 });
 
 
